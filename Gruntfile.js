d886e506bb842b1fef3c4e3559e238f749c3d166
Lanfei <CooLanfei@163.com>
2015-05-12 11:45:36 +0800

    Update version to 1.2.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,17 @@
 module.exports = function(grunt) {
 
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json')
-	});
-
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 
 	grunt.registerTask('default', function() {
 
 		grunt.initConfig({
+			pkg: grunt.file.readJSON('package.json'),
 			uglify: {
 				default: {
+					options: {
+						// sourceMap: true,
+						banner: '/*! <%= pkg.name %> <%= pkg.version %> | <%= pkg.description %> */\n'
+					},
 					files: {
 						'./src/gotpl.min.js': './src/gotpl.js'
 					}
