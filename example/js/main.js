074524c8677ec7d52e6c785e2766cd12f34d23ed
Jealous <CooLanfei@163.com>
2018-02-27 16:44:00 +0800

    Refactor codes, release v6.0.0

---
+++ example/js/main.js
@@ -0,0 +1,37 @@
+(function () {
+	var result = document.getElementById('result');
+	var tplEditor = ace.edit("template");
+	tplEditor.setTheme("ace/theme/github");
+	tplEditor.getSession().setMode("ace/mode/ejs");
+	tplEditor.setHighlightActiveLine(false);
+	tplEditor.on('change', render);
+	var dataEditor = ace.edit("data");
+	dataEditor.setTheme("ace/theme/github");
+	dataEditor.getSession().setMode("ace/mode/json");
+	dataEditor.setHighlightActiveLine(false);
+	dataEditor.on('change', render);
+
+	gotpl.config({
+		debug: true
+	});
+
+	function render() {
+		var tpl = tplEditor.getValue();
+		var json = dataEditor.getValue();
+		try {
+			var data = JSON.parse(json);
+			result.innerHTML = gotpl.render(tpl, data);
+		} catch (e) {
+			console.error(e);
+			result.innerHTML = '<pre>' + htmlEncode(e.message) + '</pre>';
+		}
+	}
+
+	function htmlEncode(str) {
+		var ele = document.createElement('div');
+		ele.appendChild(document.createTextNode(str));
+		return ele.innerHTML;
+	}
+
+	render();
+})();
